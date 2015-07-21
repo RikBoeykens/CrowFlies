a31cb98795e55a113b82dd0b1aaecb7256dbdeda
RikBoeykens <rikboeykens@gmail.com>
2015-07-21 20:51:20 +0100

    Cleanup

--- www/js/services/apiAviaryRepository.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var app = angular.module("crowFlies");
-
-app.service('apiAviaryRepository', function ($http) {
-	this.getAviary = function(aviaryid){
-		return $http.get('https://frozen-tor-7116.herokuapp.com/api/v1/aviaries/'+ aviaryid + '.json')
-	}
-});
